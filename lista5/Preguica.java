fba1d92f300274fcfda0821304a87bea1ac511dd
sabrinalvespaiva <sabrina.paiva14@hotmail.com>
2021-08-09 20:25:03 -0300

    exercicios aula 09/08

---
+++ lista5/Preguica.java
@@ -0,0 +1,35 @@
+package lista5;
+
+public class Preguica extends Animal implements Sons{
+	
+private boolean subirArvore;
+	
+	//CONSTRUTOR 
+	public Preguica(String nome, int idade, String som) {
+		//super é a mãe, fez o contrutor herdar os atibutos de Produto
+		super(nome, idade, som);
+		this.subirArvore = subirArvore;
+	}	
+
+	public boolean getSubirArvore() {
+		return subirArvore;
+	}
+
+
+	public void setSubirArvore(boolean subirArvore) {
+		this.subirArvore = subirArvore;
+	}
+
+
+	@Override
+	public void somAlto() {
+		System.out.println("GRITINHOS");
+	}
+
+	@Override
+	public void somBaixo() {
+		System.out.println("gritinhos");
+	}
+	
+
+}
